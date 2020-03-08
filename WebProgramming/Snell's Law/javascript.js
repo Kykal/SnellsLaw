22b0ff0f820204392d3991c24ede9539abfd21f3
Kykal <benavides.alan@hotmail.com>
2020-03-08 16:00:29 -0600

    Added text, selectors option and little options selected response.

--- WebProgramming/Snell's Law/javascript.js
+++ WebProgramming/Snell's Law/javascript.js
@@ -1,81 +1,13 @@
-function calc(){
-    var n1 = parseFloat(document.getElementById('n1').value);
-    var ti = parseFloat(document.getElementById('ti').value);
-    var n2 = parseFloat(document.getElementById('n2').value);
-    var tr = parseFloat(document.getElementById('tr').value);
+function select(){
 
-    if (document.getElementById('resultados').selectedIndex == 0){  //n1
-
-        document.getElementById('pn1').style.backgroundColor='mediumseagreen';
-
-        document.getElementById('imgn1').style.display='block';
-        document.getElementById('imgn2').style.display='none';
-        document.getElementById('imgti').style.display='none';
-        document.getElementById('imgtr').style.display='none';
-
-        document.getElementById('n1r').value = 0;
-        document.getElementById('tir').value = 0;
-        document.getElementById('n2r').value = 0;
-        document.getElementById('trr').value = 0;
-
-    } else if (document.getElementById('resultados').selectedIndex == 1) {  //tI
-        
-        document.getElementById('pn1').style.backgroundColor='mediumseagreen';
-        document.getElementById('pti').style.backgroundColor='white';
-        document.getElementById('pn2').style.backgroundColor='mediumseagreen';
-        document.getElementById('ptr').style.backgroundColor='mediumseagreen';
-
-
-        document.getElementById('imgn1').style.display='none';
-        document.getElementById('imgn2').style.display='none';
-        document.getElementById('imgti').style.display='block';
-        document.getElementById('imgtr').style.display='none';
-
-        document.getElementById('n1r').value = 0;
-        document.getElementById('tir').value = 0;
-        document.getElementById('n2r').value = 0;
-        document.getElementById('trr').value = 0;
-
-    } else if (document.getElementById('resultados').selectedIndex == 2) {  //n2
-        
-        document.getElementById('pn1').style.backgroundColor='mediumseagreen';
-        document.getElementById('pti').style.backgroundColor='mediumseagreen';
-        document.getElementById('pn2').style.backgroundColor='white';
-        document.getElementById('ptr').style.backgroundColor='mediumseagreen';
-
-        document.getElementById('imgn1').style.display='none';
-        document.getElementById('imgn2').style.display='block';
-        document.getElementById('imgti').style.display='none';
-        document.getElementById('imgtr').style.display='none';
-
-
-
-        document.getElementById('n1r').value = 0;
-        document.getElementById('tir').value = 0;
-        document.getElementById('n2r').value = 0;
-        document.getElementById('trr').value = 0;
-
-    } else if (document.getElementById('resultados').selectedIndex == 3) {  //tR
-        
-        document.getElementById('pn1').style.backgroundColor='mediumseagreen';
-        document.getElementById('pn1').style.backgroundColor.sub='mediumseagreen';
-        document.getElementById('pti').style.backgroundColor='mediumseagreen';
-        document.getElementById('pn2').style.backgroundColor='mediumseagreen';
-        document.getElementById('ptr').style.backgroundColor='white';
-
-        document.getElementById('imgn1').style.display='none';
-        document.getElementById('imgn2').style.display='none';
-        document.getElementById('imgti').style.display='none';
-        document.getElementById('imgtr').style.display='block';
-        
-        var DEG1 = ( ti*( Math.PI/180 ) );   //ti RADIAN -> ti DEGREES
-        var DEG2 = ( n1*(Math.sin(DEG1)/n2));
-        
-        var trr = (Math.asin(DEG2))*(180/Math.PI);
-
-        document.getElementById('n1r').value = 0;
-        document.getElementById('tir').value = 0;
-        document.getElementById('n2r').value = 0;
-        document.getElementById('trr').value = trr;
+    if(document.getElementById('values').selectedIndex == 0){
+        alert('n1');
+    } else if(document.getElementById('values').selectedIndex == 1){
+        alert('θI');
+    } else if(document.getElementById('values').selectedIndex == 2){
+        alert('n2');
+    } else if(document.getElementById('values').selectedIndex == 3){
+        alert('θR');
     }
+
 }
